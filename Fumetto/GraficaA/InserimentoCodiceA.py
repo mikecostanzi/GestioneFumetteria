00537f6c30bb22c61f723ee85cf60f2131b622fe
BoschettiMarco <s1097718@studenti.univpm.it>
2022-08-30 18:33:06 +0200

    Male molto male

--- Fumetto/GraficaA/InserimentoCodiceA.py
+++ Fumetto/GraficaA/InserimentoCodiceA.py
@@ -1,23 +1,24 @@
-from PyQt5.QtWidgets import QWidget, QGridLayout, QLabel, QLineEdit,QPushButton
+import os.path
+import json
 
+from PyQt5.QtGui import QStandardItemModel, QStandardItem
+from PyQt5.QtWidgets import QWidget, QHBoxLayout, QListView, QVBoxLayout, QPushButton, QGridLayout, QLabel, QLineEdit
+from Fumetto.GraficaA.VistaFumetto import VistaFumetto
+from Fumetto.Classi.FumettoAcquistabile import FumettoAcquistabile
 
 class InserimentoCodiceA(QWidget):
 
-    def __int__(self,parent=None):
-        super(InserimentoCodiceA, self).__int__(parent)
+    def __init__(self, parent=None):
+        super(InserimentoCodiceA, self).__init__(parent)
 
         self.grid_layout = QGridLayout()
         self.dato = QLabel("Digita il codice")
-        self.codice = QLineEdit()
-        self.button = QPushButton("Avvia Ricerca")
-        self.button.clicked.connect(self.load_fumetti)
-        self.grid_layout.addWidget(self.dato, 0, 1)
-        self.grid_layout.addWidget(self.codice,0,2)
-        self.grid_layout.addWidget(self.button,0,3)
-        self.setLayout(self.grid_layout)
-        self.resize(500,500)
-        self.setWindowTitle("Elimina/Modifica")
 
+        self.grid_layout.addWidget(self.dato, 1, 1)
+
+        self.grid_layout.setLayout(self.grid_layout)
+        self.resize(600, 300)
+        self.setWindowTitle("Gestisci Clienti")
 
     def load_fumetti(self):
         pass
